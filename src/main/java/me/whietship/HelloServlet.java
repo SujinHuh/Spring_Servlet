b5af7f086effd66c3b3175782d887bb5da6dce44
수진저장소 <43295876+SujinHuh@users.noreply.github.com>
2019-12-10 16:06:11 +0900

    ServletListener

--- src/main/java/me/whietship/HelloServlet.java
+++ src/main/java/me/whietship/HelloServlet.java
@@ -10,23 +10,27 @@ public class HelloServlet extends HttpServlet {
 
     @Override
     public void init() throws ServletException {
-        System.out.print("Go init");
+        System.out.println("Go init");
     }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        System.out.print("do Get");
+        System.out.println("do Get");
         resp.getWriter().println("<html>");
         resp.getWriter().println("<head>");
         resp.getWriter().println("<body>");
-        resp.getWriter().println("<h1>Hi Servlet</h1>");
+        resp.getWriter().println("<h1>Hi,"+ getName() +"</h1>");
         resp.getWriter().println("</body>");
         resp.getWriter().println("</head>");
         resp.getWriter().println("</html>");
     }
 
+    private Object getName() {
+        return getServletContext().getAttribute("name");
+    }
+
     @Override
     public void destroy() {
-        System.out.print("destroy");
+        System.out.println("destroy");
     }
 }
