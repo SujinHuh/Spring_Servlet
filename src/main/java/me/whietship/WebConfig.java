2dff52fffab8daa0031df8ce22fb38b1a73c9233
수진저장소 <43295876+SujinHuh@users.noreply.github.com>
2019-12-11 12:08:24 +0900

    Dispatcher Servlet(스프링 MVC 연동)  - 서블릿 애플리케이션에 스프링 연동하기 : 스프링이 제공하는 서블릿 구현체 DispatcherServlet 사용하기                                        - 계층구조 만들 필요없이                                          DispatcherServlet 에서 모든 빈을 등록함
                                             WebConfig  설정파일로 모든 빈을 등록을 함
                                             Web.xml에서 Context Loder, Lisener 없이 사용

--- src/main/java/me/whietship/WebConfig.java
+++ src/main/java/me/whietship/WebConfig.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Controller;
 
 @Configuration
-@ComponentScan(useDefaultFilters = false, includeFilters = @ComponentScan.Filter(Controller.class))
-//defalt filter사용하지 않고,Controller만 빈으로 등록하겠다.
+@ComponentScan
 public class WebConfig {
 }
