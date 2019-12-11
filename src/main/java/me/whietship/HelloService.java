496a4ce21989496b4aa55717327a096fd7bd5160
수진저장소 <43295876+SujinHuh@users.noreply.github.com>
2019-12-11 10:02:18 +0900

    스프링 IoC 컨테이너 연동  -서블릿 애플리케이션에 스프링 연동 : 서블릿에서 스프링이 제공하는 IoC 컨테이너 활용하는 방법

---
+++ src/main/java/me/whietship/HelloService.java
@@ -0,0 +1,11 @@
+package me.whietship;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class HelloService {
+
+    public String getName(){
+        return "whiteship";
+    }
+}
