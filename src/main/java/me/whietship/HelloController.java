9be5edb404ceeea198a5d4fd416fcdff12261a1a
수진저장소 <43295876+SujinHuh@users.noreply.github.com>
2019-12-11 12:57:51 +0900

    Dispatcher Servlet 동작 return 값따라 달라진 : Model and View가 Null값, Model and View가 NotNull                                            (ResponsBody가 없는경우 retrun view의 이름으로 인식)

--- src/main/java/me/whietship/HelloController.java
+++ src/main/java/me/whietship/HelloController.java
@@ -1,17 +1,28 @@
 package me.whietship;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HelloController {
 
     @Autowired
     HelloService helloService;
 
     @GetMapping("/hello")
+    @ResponseBody// ResponseBody를 통해서 반환받은 값
+    //Model and View가 Null값
     public String hello() {
         return "Hello, "  + helloService.getName();
     }
+
+    @GetMapping("/sample")//ResponseBody를 없이 문자열만 반환받은 값
+    //Model and View가 NotNull
+    public String sample() {
+        return "/WEB-INF/sample.jsp";
+    }
+
 }
