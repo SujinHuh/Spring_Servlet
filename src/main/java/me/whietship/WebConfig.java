3bd9f74b1e01925f3a20229a5c92cb7a1c80d997
수진저장소 <43295876+SujinHuh@users.noreply.github.com>
2019-12-11 11:57:55 +0900

    Dispatcher Servlet(스프링 MVC 연동)  - 서블릿 애플리케이션에 스프링 연동하기 : 스프링이 제공하는 서블릿 구현체 DispatcherServlet 사용하기

--- src/main/java/me/whietship/WebConfig.java
+++ src/main/java/me/whietship/WebConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Controller;
 
 @Configuration
-@ComponentScan(excludeFilters = @ComponentScan.Filter(Controller.class)) //Controller는 bean으로 등록하지 않겠다.
-public class AppConfig {
+@ComponentScan(useDefaultFilters = false, includeFilters = @ComponentScan.Filter(Controller.class))
+//defalt filter사용하지 않고,Controller만 빈으로 등록하겠다.
+public class WebConfig {
 }
